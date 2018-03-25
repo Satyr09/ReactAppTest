a0cf1f60de5bbda6cc0e1180328fa6e08f216f8d
Daipayan Mukherjee <30791985+Satyr09@users.noreply.github.com>
2018-03-25 13:06:54 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,65 @@
+import React from "react";
+import './styles.css';
+import Welcome from './header.js';
+import ReactDOM from 'react-dom';
+
+import $ from 'jquery';
+
+export default class Header extends React.Component{
+constructor(props){
+  super(props);
+  this.state={
+    repodatas : " " ,
+    userdatas :  " ",
+
+  };
+}
+  render(){
+    let handleSubmit= ((e)=>{
+   
+      e.preventDefault();
+      var user=($('#username').val());
+          this.setState({userdatas : user});
+      var repo = ($('#reponame').val());
+      this.setState({repodatas : repo});
+          console.log(user+" "+repo);
+    
+    });
+
+      return(
+      <div className ="wrapperclass">
+        <h1> Fetch users assgined issues </h1>
+        <h3> In React </h3>
+
+
+        <div id="searchform">
+            <form id="myForm" name="myForm" onSubmit = {handleSubmit}>
+             <div>
+                    <label >Enter github username:</label>
+                    <input type="text" id="username" name="username"/>
+                    <br/>
+                    <br/>
+                    <br/>
+                     <label >Enter a repository of this user:</label>
+                    <input type="text" id="reponame" name="reponame"/>
+             </div>
+             <br/>
+             <br/>
+             <br/>
+  
+        <input class="button" type="submit" value="Submit!"/>
+        </form>
+          <Welcome repodata = {this.state.repodatas}  userdata= {this.state.userdatas} />
+        </div>
+        </div>
+        );
+
+
+  }
+
+}
+
+ReactDOM.render(
+   <Header/>,
+   document.getElementById('root')
+);
